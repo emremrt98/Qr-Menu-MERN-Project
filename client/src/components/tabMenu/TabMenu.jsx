fdb3fd32749823df3969f3ee6db5caff1253a3aa
emre mert <emre.mrt98@gmail.com>
2023-06-29 13:08:28 +0300

    Fix the tab menu's problem

--- client/src/components/tabMenu/TabMenu.jsx
+++ client/src/components/tabMenu/TabMenu.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function TabMenu() {
+export default function TabMenu({ setSelectButton, selectButton }) {
+
+    const handleChange = (e) => {
+        const dataNum = e.target.getAttribute('data-num');
+        setSelectButton(dataNum);
+    }
+
     return (
 
         <>
-            <input type="radio" name='menu' id="ana-yemek" hidden />
-            <input type="radio" name='menu' id="ara-yemek" hidden />
-            <input type="radio" name='menu' id="icecek" hidden />
-            <input type="radio" name='menu' id="tatli" hidden />
+            <input onChange={handleChange} type="radio" name='menu' id="ana-yemek" data-num="1" hidden />
+            <input onChange={handleChange} type="radio" name='menu' id="ara-yemek" data-num="2" hidden />
+            <input onChange={handleChange} type="radio" name='menu' id="icecek" data-num="3" hidden />
+            <input onChange={handleChange} type="radio" name='menu' id="tatli" data-num="4" hidden />
             <div className='tab-menu'>
-                <label id='main-food' htmlFor="ana-yemek">Ana Yemek</label>
-                <label id='snack' htmlFor="ara-yemek">Ara Yemek</label>
-                <label id='drinks' htmlFor="icecek">İçecek</label>
-                <label id='dessert' htmlFor="tatli">Tatlı</label>
+                <label style={selectButton == '1' ? { color: "#FFE066" } : { color: "#FFFFFF" }} id='main-food' htmlFor="ana-yemek">Ana Yemek</label>
+                <label style={selectButton == '2' ? { color: "#FFE066" } : { color: "#FFFFFF" }} id='snack' htmlFor="ara-yemek">Ara Yemek</label>
+                <label style={selectButton == '3' ? { color: "#FFE066" } : { color: "#FFFFFF" }} id='drinks' htmlFor="icecek">İçecek</label>
+                <label style={selectButton == '4' ? { color: "#FFE066" } : { color: "#FFFFFF" }} id='dessert' htmlFor="tatli">Tatlı</label>
             </div>
 
         </>
--- client/src/pages/food/Food.jsx
+++ client/src/pages/food/Food.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TabMenu from '../../components/tabMenu/TabMenu'
 export default function Food() {
+  const [selectButton, setSelectButton] = useState("1");
+
+  useEffect(() => { console.log(selectButton) }, [selectButton])
+
+
   return (
     <div className='food'>
-      <TabMenu />
+      <TabMenu setSelectButton={setSelectButton} selectButton={selectButton} />
       <div className='menus'>
-        <div className='main-food'>Ana yemek</div>
-        <div className='snack'>Ara Yemek</div>
-        <div className='drinks'>İçecek</div>
-        <div className='dessert'>Tatlı</div>
+        <div style={selectButton == '1' ? { display: "block" } : { display: "none" }} className='main-food'>Ana yemek</div>
+        <div style={selectButton == '2' ? { display: "block" } : { display: "none" }} className='snack'>Ara Yemek</div>
+        <div style={selectButton == '3' ? { display: "block" } : { display: "none" }} className='drinks'>İçecek</div>
+        <div style={selectButton == '4' ? { display: "block" } : { display: "none" }} className='dessert'>Tatlı</div>
       </div>
     </div>
   )
